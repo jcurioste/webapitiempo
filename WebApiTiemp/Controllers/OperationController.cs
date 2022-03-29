3dc909c95164296539009c0ff81f94d3b6c33c5d
jaime urioste <a2020114313@estudiantes.upsa.edu.bo>
2022-03-28 21:19:49 -0400

    Agregar archivos de proyecto.

---
+++ WebApiTiemp/Controllers/OperationController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTiemp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Sum")]
+
+        public int Sum(
+            int numa,
+            int numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Sum")]
+
+        public int SumPost(
+            [FromHeader] int numa,
+            [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+        [HttpGet]
+        [Route("Mul")]
+
+        public int Mul(
+            int numa,
+            int numb)
+        {
+            return numa * numb;
+        }
+        [HttpPost]
+        [Route("Mul")]
+
+        public int MulPost(
+            [FromHeader] int numa,
+            [FromHeader] int numb)
+        {
+            return numa * numb;
+        }
+        [HttpGet]
+        [Route("Res")]
+        public int Res(
+            int numa,
+            int numb)
+        {
+            return numa - numb;
+        }
+        [HttpPost]
+        [Route("Res")]
+
+        public int ResPost(
+            [FromHeader] int numa,
+            [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+        [HttpGet]
+        [Route("Div")]
+
+        public int Div(
+            int numa,
+            int numb)
+        {
+            return numa / numb;
+        }
+        [HttpPost]
+        [Route("Div")]
+
+        public int DivPost(
+            [FromHeader] int numa,
+            [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+    }
+}
